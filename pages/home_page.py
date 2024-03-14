2846b22416b2c59da80901668abad16b7373086a
Vladimir Smolko <isendel@yahoo.com>
2024-03-14 21:31:43 +0400

    adding sorting methods to home page object

--- pages/home_page.py
+++ pages/home_page.py
@@ -2,6 +2,7 @@ from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 
 class HomePage(BasePage):
@@ -16,19 +17,29 @@ class HomePage(BasePage):
     RESET_SEARCH_BTN = (By.CSS_SELECTOR, 'button.btn:nth-child(2)')
     SEARCH_RESULTS_HEADER = (By.CSS_SELECTOR, '.col-md-9 > h3:nth-child(1)')
     CATALOG_CONTAINER = (By.CSS_SELECTOR, '.col-md-9 > div:nth-child(2)')
+    SORT_INPUT = (By.CSS_SELECTOR, '.form-select')
 
     def __init__(self, driver):
         super().__init__(driver)
 
-    def wait_for_search_to_complete(self, timeout=5):
+    def wait_for_search_to_complete(self, timeout=5, attribute='search_completed'):
         """ Wait until catalog container fains attribute search_completed """
         WebDriverWait(self.driver, timeout).until(
-            EC.text_to_be_present_in_element_attribute(self.CATALOG_CONTAINER, 'data-test', 'search_completed')
+            EC.text_to_be_present_in_element_attribute(self.CATALOG_CONTAINER, 'data-test', attribute)
         )
 
     def wait_for_search_reset(self, timeout=5):
         """ Wait until search results are reset: container element reloads and visible again """
         WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(self.CATALOG_CONTAINER))
+
+    def wait_for_next_catalog_page(self, timeout=10):
+        """ Wait for products displayed on the page to be updated when navigating to next pagination item
+            Checks if previous catalog page is stale
+            Default timeout for wait is 10s. Custom timeout can be passed as argument
+        """
+        WebDriverWait(self.driver, timeout).until(
+            EC.staleness_of(self.driver.find_element(*self.PRODUCT_CARDS))
+        )
 
     def go_to_next_page(self):
         """ Go to the nexxt page of the product catalog """
@@ -58,6 +69,17 @@ class HomePage(BasePage):
                 )
         return cards_info
 
+    def get_all_product_cards(self):
+        """  Method clicks through all the pages of the catalog and gathers all products as a list of dictionaries """
+        pagination_items = self.wait_for_elements(self.PAGINATION_ITEMS)
+        cards = []
+        for i in range(len(pagination_items) - 2):
+            self.wait_for_next_catalog_page()
+            cards.extend(self.get_product_cards())
+            if i < len(pagination_items) - 3:
+                self.go_to_next_page()
+        return cards
+
     def open_product(self, name):
         """ Open product with the specified name """
         cards = self.get_product_cards()
@@ -80,3 +102,23 @@ class HomePage(BasePage):
         reset_btn = self.wait_for_element(self.RESET_SEARCH_BTN)
         reset_btn.click()
         self.wait_for_search_reset()
+
+    def sort_alphabetically_asc(self):
+        """ Sorts products by name in ascending order """
+        sort_select = Select(self.wait_for_element(self.SORT_INPUT))
+        sort_select.select_by_visible_text('Name (A - Z)')
+
+    def sort_alphabetically_desc(self):
+        """ Sorts products by name in descending order """
+        sort_select = Select(self.wait_for_element(self.SORT_INPUT))
+        sort_select.select_by_visible_text('Name (Z - A)')
+
+    def sort_price_desc(self):
+        """ Sorts products by price in ascending order """
+        sort_select = Select(self.wait_for_element(self.SORT_INPUT))
+        sort_select.select_by_visible_text('Price (High - Low)')
+
+    def sort_price_asc(self):
+        """ Sorts products by price in ascending descending """
+        sort_select = Select(self.wait_for_element(self.SORT_INPUT))
+        sort_select.select_by_visible_text('Price (Low - High)')
